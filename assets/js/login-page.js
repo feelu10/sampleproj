68f3b08b3fb15233ebb9f8664e93bceedabee6b3
Carl James <122439576+feelu10@users.noreply.github.com>
2023-11-15 21:42:08 +0800

    Update login-page.js

--- assets/js/login-page.js
+++ assets/js/login-page.js
@@ -1,58 +1,57 @@
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  // Get a reference to the login form by its ID
-  const loginForm = document.getElementById("login-form");
+ // Wait for the DOM to be fully loaded
+  document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the login form by its ID
+    const loginForm = document.getElementById("login-form");
 
-  if (loginForm) {
-    // Get references to the input fields
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("psw");
+    if (loginForm) {
+      // Get references to the input fields
+      const emailInput = document.getElementById("email");
+      const passwordInput = document.getElementById("psw");
 
-    // Add an event listener for the form submission
-    loginForm.addEventListener("submit", async function (e) {
-      e.preventDefault(); // Prevent the default form submission
+      // Add an event listener for the form submission
+      loginForm.addEventListener("submit", async function (e) {
+        e.preventDefault(); // Prevent the default form submission
 
-      // Get the user's email and password from the input fields
-      const email = emailInput.value;
-      const password = passwordInput.value;
+        // Get the user's email and password from the input fields
+        const email = emailInput.value;
+        const password = passwordInput.value;
 
-      console.log("Login form submitted:", { email, password });
+        console.log("Login form submitted:", { email, password });
 
-      // Send a POST request to your server with the login data
-      try {
-        const response = await fetch("/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
-        });
+        // Send a POST request to your server with the login data
+        try {
+          const response = await fetch("/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+          });
 
-        console.log("Login response:", response);
+          console.log("Login response:", response);
 
-        if (!response.ok) {
-          // Handle login failure
-          console.error("Error during login:", response.statusText);
-          return;
+          if (!response.ok) {
+            // Handle login failure
+            console.error("Error during login:", response.statusText);
+            return;
+          }
+
+          // Parse the JSON response
+          const data = await response.json();
+
+          console.log("Login data:", data);
+
+          // Check the user's account type and redirect accordingly
+          if (data.accountType === "Student") {
+            window.location.href = "/StudentHomepage";
+          } else if (data.accountType === "Counselor") {
+            window.location.href = "/CounselorHomepage";
+          } else {
+            console.error("Unknown account type:", data.accountType);
+          }
+        } catch (error) {
+          console.error("Error during login:", error);
         }
-
-        // Parse the JSON response
-        const data = await response.json();
-
-        console.log("Login data:", data);
-
-        // Check the user's account type and redirect accordingly
-        if (data.accountType === "Student") {
-          window.location.href = "/StudentHomepage"; // Redirect to student homepage
-        } else if (data.accountType === "Counselor") {
-          window.location.href = "/CounselorHomepage"; // Redirect to counselor homepage
-        } else {
-          console.error("Unknown account type:", data.accountType);
-        }
-      } catch (error) {
-        console.error("Error during login:", error);
-      }
-    });
-  }
-});
-  
+      });
+    }
+  });
