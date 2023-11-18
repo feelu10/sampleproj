34406d360a2692ae659d59964b040fb3e6561d3b
Carl James <122439576+feelu10@users.noreply.github.com>
2023-11-18 13:03:16 +0800

    Update login-page.js

--- assets/js/login-page.js
+++ assets/js/login-page.js
@@ -1,4 +1,3 @@
-// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
   // Get a reference to the login form by its ID
   const loginForm = document.getElementById("login-form");
@@ -12,6 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
     loginForm.addEventListener("submit", async function (e) {
       e.preventDefault(); // Prevent the default form submission
 
+      // Show the loading screen
+      showLoadingScreen();
+
       // Get the user's email and password from the input fields
       const email = emailInput.value;
       const password = passwordInput.value;
@@ -19,37 +21,59 @@ document.addEventListener("DOMContentLoaded", function () {
       // Send a POST request to your server with the login data
       try {
         const response = await fetch("/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
         });
-
-        // Log the response text for debugging
-        const responseText = await response.text();
-        console.log('Response from server:', responseText);
-
+    
         if (!response.ok) {
-          // Handle login failure
-          console.error('Error during login:', response.statusText);
-          return;
+            // Handle login failure
+            console.error('Error during login:', response.statusText);
+            // Hide the loading screen in case of an error
+            hideLoadingScreen();
+            return;
         }
-
-        // Parse the JSON response
-        const data = await response.json();
-
-        // Check the user's account type and redirect accordingly
-        if (data.accountType === 'Student') {
-          window.location.href = '/StudentHomepage'; // Redirect to student homepage
-        } else if (data.accountType === 'Counselor') {
-          window.location.href = '/CounselorHomepage'; // Redirect to counselor homepage
+    
+        // Check if the response has a JSON content type before reading
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.includes('application/json')) {
+            // Parse the JSON response
+            const data = await response.json();
+    
+            // Check the user's account type and redirect accordingly
+            if (data.accountType === 'Student') {
+                window.location.href = '/StudentHomepage'; // Redirect to student homepage
+            } else if (data.accountType === 'Counselor') {
+                window.location.href = '/CounselorHomepage'; // Redirect to counselor homepage
+            } else {
+                console.error('Unknown account type:', data.accountType);
+            }
         } else {
-          console.error('Unknown account type:', data.accountType);
+            console.error('Unexpected response format. Expected JSON.');
         }
-      } catch (error) {
+    } catch (error) {
         console.error('Error during login:', error);
-      }
+        // Hide the loading screen in case of an error
+        hideLoadingScreen();
+    }
     });
   }
 });
+
+// Function to show the loading screen
+function showLoadingScreen() {
+  const loadingScreen = document.getElementById("loading-screen");
+  if (loadingScreen) {
+    loadingScreen.style.display = "flex";
+  }
+}
+
+// Function to hide the loading screen
+function hideLoadingScreen() {
+  const loadingScreen = document.getElementById("loading-screen");
+  if (loadingScreen) {
+    loadingScreen.style.display = "none";
+  }
+}
