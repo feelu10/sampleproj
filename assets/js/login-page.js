cb426e9033ac03b4efa3e1bde0e80021e6744814
AdrianVyne <imadrian1234@gmail.com>
2023-11-18 18:45:24 +0800

    asas

--- assets/js/login-page.js
+++ assets/js/login-page.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
           sessionStorage.setItem('studentData', JSON.stringify(data.studentData));
           window.location.href = '/StudentHomepage'; // Redirect to student homepage
         } else if (data.accountType === 'Counselor') {
-          sessionStorage.setItem('counselorData', JSON.stringify(data.counserloData));
+          sessionStorage.setItem('counselorData', JSON.stringify(data.counselorData));
           window.location.href = '/CounselorHomepage'; // Redirect to counselor homepage
         } else {
           console.error('Unknown account type:', data.accountType);
